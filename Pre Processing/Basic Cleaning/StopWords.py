8a9fc714d35b3e93e6840b6a6dac022bcec25e68
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-04-05 07:47:28 +0530

    Create StopWords.py

---
+++ Pre Processing/Basic Cleaning/StopWords.py
@@ -0,0 +1,18 @@
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stopwords.words('english')
+
+def remove_stopwords(text):
+    new_text = []
+    
+    for word in text.split():
+        if word in stopwords.words('english'):
+            new_text.append('')
+        else:
+            new_text.append(word)
+    x = new_text[:]
+    new_text.clear()
+    return " ".join(x)
+    
+remove_stopwords('probably my all-time favorite movie, a story of selflessness, sacrifice and dedication to a noble cause, but it\'s not preachy or boring. it just never gets old, despite my having seen it some 15 or more times')
